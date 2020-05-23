68c8a962e94b0bfc539d827b072da6640634fbe7
NovaisGabriel <gabrieel.novais@gmail.com>
2020-05-23 17:18:06 -0300

    ItemList Class

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -11,12 +11,21 @@ class Item(Resource):
         for item in items:
             if item['name'] == name:
                 return item
+        return {'item':None}, 404
 
     def post(self, name):
-        item = {'name': name, 'price': 12.00}
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
         items.append(item)
-        return item
-    
-api.add_resource(Item, '/item/<string:name>')
+        return item, 201
 
-app.run(port=2000)
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+    
+
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=2000, debug=True)
